b76c5a1bc25eab673268ce25c8838c1cbd8b77c9
Brett Ian Balogh <giantmolecules@gmail.com>
2016-09-27 15:08:58 -0500

    Update probe.py

--- probe.py
+++ probe.py
@@ -1,55 +1,95 @@
+#!/usr/bin/python
+
+import time
+import datetime
+import argparse
+import netaddr
+import sys
+import logging
 from scapy.all import *
-import RPi.GPIO as GPIO
-GPIO.setmode(GPIO.BCM)
-GPIO.setup(14, GPIO.OUT)
-p = GPIO.PWM(14, 100);
-p.start(0);
-PROBE_REQUEST_TYPE=0
-PROBE_REQUEST_SUBTYPE=4
+from pprint import pprint
+from logging.handlers import RotatingFileHandler
 
-addressAge = 10
-pwmValue = 0
-deviceList = []
-deviceDict = {}
-dictLength = 0
 
-def PacketHandler(pkt):
-    if pkt.haslayer(Dot11):
-        if pkt.type==PROBE_REQUEST_TYPE and pkt.subtype == PROBE_REQUEST_SUBTYPE:
-            PrintPacket(pkt)
+NAME = 'probemon'
+DESCRIPTION = "a command line tool for logging 802.11 probe request frames"
 
-def PrintPacket(pkt):
-    print "Probe Request Captured:"
-    try:
-        extra = pkt.notdecoded
-    except:
-        extra = None
-    if extra!=None:
-        signal_strength = -(256-ord(extra[-4:-3]))
-    else:
-        signal_strength = -100
-        print "No signal strength found"    
-    print "Target: %s Source: %s SSID: %s RSSi: %d"%(pkt.addr3,pkt.addr2,pkt.getlayer(Dot11ProbeReq).info,signal_strength)
-    if pkt.addr2 not in deviceList:
-        deviceList.append(pkt.addr2)
-    print deviceList
-    if pkt.addr2 not in deviceDict:
-        deviceDict[pkt.addr2]=time.time()
-    for address, timestamp in deviceDict.items():
-        if time.time()-timestamp > addressAge:
-            del deviceDict[address]
-    print len(deviceDict)
-    dictLength = len(deviceDict)
-    if dictLength > 10:
-        dictLength = 10
-    pwmValue = (100/10)*dictLength
-    p.ChangeDutyCycle(pwmValue)
+DEBUG = False
+
+def build_packet_callback(time_fmt, logger, delimiter, mac_info, ssid, rssi):
+	def packet_callback(packet):
+		
+		if not packet.haslayer(Dot11):
+			return
+
+		# we are looking for management frames with a probe subtype
+		# if neither match we are done here
+		if packet.type != 0 or packet.subtype != 0x04:
+			return
+
+		# list of output fields
+		fields = []
+
+		# determine preferred time format 
+		log_time = str(int(time.time()))
+		if time_fmt == 'iso':
+			log_time = datetime.datetime.now().isoformat()
+
+		fields.append(log_time)
+
+		# append the mac address itself
+		fields.append(packet.addr2)
+
+		# parse mac address and look up the organization from the vendor octets
+		if mac_info:
+			try:
+				parsed_mac = netaddr.EUI(packet.addr2)
+				fields.append(parsed_mac.oui.registration().org)
+			except netaddr.core.NotRegisteredError, e:
+				fields.append('UNKNOWN')
+
+		# include the SSID in the probe frame
+		if ssid:
+			fields.append(packet.info)
+			
+		if rssi:
+			rssi_val = -(256-ord(packet.notdecoded[-4:-3]))
+			fields.append(str(rssi_val))
+
+		logger.info(delimiter.join(fields))
+
+	return packet_callback
 
 def main():
-    from datetime import datetime
-    print "[%s] Starting scan"%time.time()
-    print "Scanning for..."
-    sniff(iface=sys.argv[1],prn=PacketHandler)
-    
-if __name__=="__main__":
-    main()
+	parser = argparse.ArgumentParser(description=DESCRIPTION)
+	parser.add_argument('-i', '--interface', help="capture interface")
+	parser.add_argument('-t', '--time', default='iso', help="output time format (unix, iso)")
+	parser.add_argument('-o', '--output', default='probemon.log', help="logging output location")
+	parser.add_argument('-b', '--max-bytes', default=5000000, help="maximum log size in bytes before rotating")
+	parser.add_argument('-c', '--max-backups', default=99999, help="maximum number of log files to keep")
+	parser.add_argument('-d', '--delimiter', default='\t', help="output field delimiter")
+	parser.add_argument('-f', '--mac-info', action='store_true', help="include MAC address manufacturer")
+	parser.add_argument('-s', '--ssid', action='store_true', help="include probe SSID in output")
+	parser.add_argument('-r', '--rssi', action='store_true', help="include rssi in output")
+	parser.add_argument('-D', '--debug', action='store_true', help="enable debug output")
+	args = parser.parse_args()
+
+	if not args.interface:
+		print "error: capture interface not given, try --help"
+		sys.exit(-1)
+	
+	DEBUG = args.debug
+
+	# setup our rotating logger
+	logger = logging.getLogger(NAME)
+	logger.setLevel(logging.INFO)
+	handler = RotatingFileHandler(args.output, maxBytes=args.max_bytes, backupCount=args.max_backups)
+	logger.addHandler(handler)
+
+	built_packet_cb = build_packet_callback(args.time, logger, 
+		args.delimiter, args.mac_info, args.ssid, args.rssi)
+	sniff(iface=args.interface, prn=built_packet_cb, store=0)
+
+
+if __name__ == '__main__':
+	main()
